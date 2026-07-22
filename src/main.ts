0f45673cb640367a86a390d2649091865d79656e
oshersa-170701 <oshersa1771@gmail.com>
2026-07-22 13:21:16 -0600

    Registrar iconos globalmente en main.ts y blindar botones para produccion

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
-import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+import { addIcons } from 'ionicons';
+import { sparkles, aperture, camera, colorPalette, qrCode, time, images, gift, chevronBack, chevronForward } from 'ionicons/icons';
 import { provideHttpClient } from '@angular/common/http';
+
+// Registrar todos los íconos globalmente para evitar que el tree-shaking los borre en producción
+addIcons({
+  'sparkles': sparkles,
+  'aperture': aperture,
+  'camera': camera,
+  'color-palette': colorPalette,
+  'qr-code': qrCode,
+  'time': time,
+  'images': images,
+  'gift': gift,
+  'chevron-back': chevronBack,
+  'chevron-forward': chevronForward
+});
 
 bootstrapApplication(AppComponent, {
   providers: [
